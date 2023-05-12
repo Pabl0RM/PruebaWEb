1496a4aef0b8cefe752fac891df539d1ffa1e23a
Pabl0RM <126168902+Pabl0RM@users.noreply.github.com>
2023-05-12 21:29:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,132 @@
-from time import sleep
+import asyncio
 import pygame
-import pygame_menu
-from pygame_menu import themes
- 
-pygame.init()
-surface = pygame.display.set_mode((600, 400))
- 
-def set_difficulty(value, difficulty):
-    print(value)
-    print(difficulty)
- 
-def start_the_game():
-    mainmenu._open(loading)
-    pygame.time.set_timer(update_loading, 30)
- 
-def level_menu():
-    mainmenu._open(level)
- 
- 
-mainmenu = pygame_menu.Menu('Welcome', 600, 400, theme=themes.THEME_SOLARIZED)
-mainmenu.add.text_input('Name: ', default='username')
-mainmenu.add.button('Play', start_the_game)
-mainmenu.add.button('Levels', level_menu)
-mainmenu.add.button('Quit', pygame_menu.events.EXIT)
- 
-level = pygame_menu.Menu('Select a Difficulty', 600, 400, theme=themes.THEME_BLUE)
-level.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
- 
-loading = pygame_menu.Menu('Loading the Game...', 600, 400, theme=themes.THEME_DARK)
-loading.add.progress_bar("Progress", progressbar_id = "1", default=0, width = 200, )
- 
-arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
- 
-update_loading = pygame.USEREVENT + 0
- 
-while True:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == update_loading:
-            progress = loading.get_widget("1")
-            progress.set_value(progress.get_value() + 1)
-            if progress.get_value() == 100:
-                pygame.time.set_timer(update_loading, 0)
-        if event.type == pygame.QUIT:
-            exit()
- 
-    if mainmenu.is_enabled():
-        mainmenu.update(events)
-        mainmenu.draw(surface)
-        if (mainmenu.get_current().get_selected_widget()):
-            arrow.draw(surface, mainmenu.get_current().get_selected_widget())
- 
-    pygame.display.update()
+import sys
+
+
+
+class Main:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode( (WIDTH, HEIGHT), pygame.NOFRAME)
+        pygame.display.set_caption('Chess')
+        self.game = Game()
+
+    async def mainloop(self):
+
+        screen = self.screen
+        game = self.game
+        board = self.game.board
+        dragger = self.game.dragger
+
+        while True:
+            # show methods
+            game.show_bg(screen)
+            game.show_last_move(screen)
+            game.show_moves(screen)
+            game.show_pieces(screen)
+            game.show_hover(screen)
+
+            if dragger.dragging:
+                dragger.update_blit(screen)
+
+            for event in pygame.event.get():
+
+                # click
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
+
+                    # if clicked square has a piece ?
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        # valid piece (color) ?
+                        if piece.color == game.next_player:
+                            board.calc_moves(piece, clicked_row, clicked_col, bool=True)
+                            dragger.save_initial(event.pos)
+                            dragger.drag_piece(piece)
+                            # show methods
+                            game.show_bg(screen)
+                            game.show_last_move(screen)
+                            game.show_moves(screen)
+                            game.show_pieces(screen)
+
+                # mouse motion
+                elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+
+                    game.set_hover(motion_row, motion_col)
+
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        # show methods
+                        game.show_bg(screen)
+                        game.show_last_move(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
+                        game.show_hover(screen)
+                        dragger.update_blit(screen)
+
+                # click release
+                elif event.type == pygame.MOUSEBUTTONUP:
+
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+
+                        released_row = dragger.mouseY // SQSIZE
+                        released_col = dragger.mouseX // SQSIZE
+
+                        # create possible move
+                        initial = Square(dragger.initial_row, dragger.initial_col)
+                        final = Square(released_row, released_col)
+                        move = Move(initial, final)
+
+                        # valid move ?
+                        if board.valid_move(dragger.piece, move):
+                            # normal capture
+                            captured = board.squares[released_row][released_col].has_piece()
+                            board.move(dragger.piece, move)
+
+                            board.set_true_en_passant(dragger.piece)
+
+                            # sounds
+                            game.play_sound(captured)
+                            # show methods
+                            game.show_bg(screen)
+                            game.show_last_move(screen)
+                            game.show_pieces(screen)
+                            # next turn
+                            game.next_turn()
+
+                    dragger.undrag_piece()
+
+                # key press
+                elif event.type == pygame.KEYDOWN:
+
+                    # changing themes
+                    if event.key == pygame.K_t:
+                        game.change_theme()
+
+                    if event.key == pygame.K_r:
+                        game.reset()
+                        game = self.game
+                        board = self.game.board
+                        dragger = self.game.dragger
+
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
+
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            await asyncio.sleep(0)
+
+if __name__ == "__main__":
+    main = Main()
+    asyncio.run(main.mainloop())
